a280a659403c9e6a12b025676e3ccf749cd8d4e3
Daniel Burkholder <danburkhol@gmail.com>
2017-07-02 15:26:20 -0400

    Updated setup to get wiring pit and 433Utils Installed

--- setup.sh
+++ setup.sh
@@ -3,9 +3,14 @@
 # Prompt for root
 ["$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
-sudo apt-get install git git-core
+sudo apt-get --yes --force-yes install git git-core
 
 # Install and build WiringPi
 git clone git://git.drogon.net/wiringPi
 cd wiringPi
 ./build
+
+# Get the 433 MHz library
+git clone --recursive git://github.com/ninjablocks/433Utils.git
+cd 433Utils/RPi_utils
+make all
